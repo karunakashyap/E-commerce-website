5673192bde19d6eaacf4f7d62578358d1aefc0a9
karuna kashyap <kashyapkaruna60@gmail.com>
2025-01-11 13:11:50 +0530

    add Item and Popular component

---
+++ src/Components/Popular/Popular.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Popular.css';
+import product_data from '../Assets/data'
+import Item from '../Item/Item';
+
+const Popular = () => {
+    return (
+        <div className='popular'>
+            <h1>POPULAR IN WOMEN</h1>
+            <hr></hr>
+            <div className="popular-item">
+                {product_data.map((item, i) => {
+                    return <Item key={i} id={item.id} name={item.name} image={item.image}
+                        new_price={item.new_price} old_price={item.old_price}></Item>
+                })}
+            </div>
+
+        </div>
+    )
+}
+
+export default Popular
